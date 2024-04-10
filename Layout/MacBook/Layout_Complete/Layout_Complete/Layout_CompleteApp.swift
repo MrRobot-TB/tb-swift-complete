d8f11018b08e33f5eded465b9c2ae5c8edf4b6ca
MrRobot-TB <harveyng.techbob@gmail.com>
2024-04-11 04:41:10 +0800

    Initialize and create layout complete in the MacBook folder

---
+++ Layout/MacBook/Layout_Complete/Layout_Complete/Layout_CompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_CompleteApp.swift
+//  Layout_Complete
+//
+//  Created by [Redacted]
+//
+
+import SwiftUI
+
+@main
+struct Layout_CompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
