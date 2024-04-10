0685adeb12718b2b61f777f7643b280b900fd4dc
MrRobot-TB <harveyng.techbob@gmail.com>
2024-04-11 06:09:12 +0800

    Initialize and create notes complete in the MacBook folder

---
+++ Notes/MacBook/Notes_Complete/Notes_Complete/NoteView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteView.swift
+//  Notes_Complete
+//
+//  Created by [Redacted]
+//
+
+import SwiftUI
+
+struct NoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteView()
+}
---
+++ Notes/MacBook/Notes_Complete/Notes_Complete/Notes_CompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_CompleteApp.swift
+//  Notes_Complete
+//
+//  Created by [Redacted]
+//
+
+import SwiftUI
+
+@main
+struct Notes_CompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
