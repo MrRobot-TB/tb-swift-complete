e2c8b74e602c595db69fa796bc4b842089d24a55
MrRobot-TB <harveyng.techbob@gmail.com>
2024-04-11 03:47:21 +0800

    Initialize and create hello world complete in the MacBook folder

---
+++ HelloWorld/MacBook/HelloWorld_Complete/HelloWorld_Complete/HelloWorld_CompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld_CompleteApp.swift
+//  HelloWorld_Complete
+//
+//  Created by [Redacted]
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld_CompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
