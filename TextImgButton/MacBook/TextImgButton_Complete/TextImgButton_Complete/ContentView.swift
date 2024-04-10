267aed36ca7486c87a006d711ad41e84ab32c92d
MrRobot-TB <harveyng.techbob@gmail.com>
2024-04-11 04:29:36 +0800

    Initialize and create text image button complete in the MacBook folder

---
+++ TextImgButton/MacBook/TextImgButton_Complete/TextImgButton_Complete/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextImgButton_Complete
+//
+//  Created by [Redacted]
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextImgButton/MacBook/TextImgButton_Complete/TextImgButton_Complete/TextImgButton_CompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextImgButton_CompleteApp.swift
+//  TextImgButton_Complete
+//
+//  Created by Harvey Ng on 11/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct TextImgButton_CompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
